947f2fbc2165b790cd939c411c730626c4581860
Justen <chongjusten@gmail.com>
2026-09-21 21:26:04 +0800

    Fix bugs

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -40,11 +40,11 @@ public class RemarkCommand extends Command {
      * @param index  of the person in the filtered person list to edit the remark
      * @param remark of the person to be updated to
      */
-    public RemarkCommand(Index index, Remark remark) {
+    public RemarkCommand(Index index, String remark) {
         requireAllNonNull(index, remark);
 
         this.index = index;
-        this.remark = remark;
+        this.remark = new Remark(remark);
     }
 
     @Override
